98e0a4f090d7e28f6fe25b93da6747d2275077a9
David Smith <cpmpdavidsmith@icloud.com>
2024-01-16 14:06:58 -0500

    prac

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace Demo
+{
+    //Calculte the total number of scores greater than 57 in serries
+    //{31-,23,64,40,85,55,50,90)
+    class SampleLambda
+    {
+        static void Main()
+        {
+            int[] scores = { -31, 23, 64, 40, 85, 55, 50, 99 };
+            int highScores = scores.Where(n => n > 57).Count();
+            Console.WriteLine("{0} scores are greater than 57" , highScores);
+            Console.Read();
+        }
+
+    }
+}
